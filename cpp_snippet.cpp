a7a0b83878afffc35b9d078025879b4557846f49
mobius29 <sagjin0000@naver.com>
2022-08-19 15:19:33 +0900

    cpp snippet

---
+++ cpp_snippet.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define sync() ios_base::sync_with_stdio(0); cin.tie(0)
+#define endl "\n"
+#define ends " "
+#define fs first
+#define se second
+#define pb push_back
+#define all(x) (x).begin, (x).end()
+#define getx(x, i) get<(x)>(i)
+using namespace std;
+
+typedef long long ll;
+typedef pair<int ,int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int>  vi;
+typedef vector<ll> vl;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tl;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, -1, 0, 1};
