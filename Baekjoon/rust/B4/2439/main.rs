8f009e6f58cc993545d72905d04996c7c5179d1d
mobius29 <sagjin0000@naver.com>
2023-08-09 17:15:33 +0900

    2439. complete with rust

---
+++ Baekjoon/rust/B4/2439/main.rs
@@ -0,0 +1,113 @@
+// https://bamgoesn.github.io/rust-ps-md/misc/fastio.html
+use std::io::Write;
+use std::str::SplitAsciiWhitespace;
+use std::{fmt, io};
+
+fn read() -> Tokenizer<SplitAsciiWhitespace<'static>> {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let str: &'static str = Box::leak(buf.into_boxed_str());
+
+    Tokenizer::new(str, |s| s.split_ascii_whitespace())
+}
+
+fn main() {
+    let line = &mut read();
+
+    let n: u32 = line.next();
+
+    let stdout = io::stdout();
+    let buf_write = &mut io::BufWriter::new(stdout.lock());
+
+    for i in 0..n {
+        for _j in 1..(n - i) {
+            write!(buf_write, " ").ok();
+        }
+
+        for _j in 0..=i {
+            write!(buf_write, "*").ok();
+        }
+
+        writeln!(buf_write).ok();
+    }
+
+    buf_write.flush().unwrap();
+}
+
+// Define Errors For Input
+pub enum InputError<'t> {
+    InputExhaust,
+    ParseError(&'t str),
+}
+
+impl<'t> fmt::Debug for InputError<'t> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputError::InputExhaust => f.debug_struct("InputExhaust").finish(),
+            InputError::ParseError(s) => f.debug_struct("ParseError").field("str", s).finish(),
+        }
+    }
+}
+
+// Implements macros for parse string to certain type
+pub trait Atom: Sized {
+    fn parse_from(s: &str) -> Result<Self, InputError>;
+}
+
+trait IterParse: Sized {
+    fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>>
+    where
+        It: Iterator<Item = &'t str>;
+}
+
+macro_rules! impl_trait_for_fromstr {
+    ($($t:ty) *) => { $(
+        impl Atom for $t {
+            fn parse_from(s: &str) -> Result<Self, InputError> {
+                s.parse().map_err(|_| InputError::ParseError(s))
+            }
+        }
+
+        impl IterParse for $t {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                it.next().map_or( Err(InputError::InputExhaust), <Self as Atom>::parse_from )
+            }
+        }
+    )* };
+}
+
+impl_trait_for_fromstr!(bool char String);
+impl_trait_for_fromstr!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64);
+
+macro_rules! impl_iterparse_for_tuple {
+    ($($t:ident) *) => {
+        impl<$($t),*> IterParse for ($($t),*) where $($t: IterParse),* {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                Ok(( $($t::parse_from(it)?),* ))
+            }
+        }
+    };
+}
+
+impl_iterparse_for_tuple!(A B);
+impl_iterparse_for_tuple!(A B C);
+impl_iterparse_for_tuple!(A B C D);
+
+// Implements Tokenizer for split string line to variables
+struct Tokenizer<It> {
+    it: It,
+}
+
+impl<'arg, 'str: 'arg, It> Tokenizer<It> {
+    pub fn new(s: &'str str, split: impl FnOnce(&'arg str) -> It) -> Self {
+        Self { it: split(s) }
+    }
+}
+
+impl<'t, It> Tokenizer<It>
+where
+    It: Iterator<Item = &'t str>,
+{
+    pub fn next<T: IterParse>(&mut self) -> T {
+        T::parse_from(&mut self.it).unwrap()
+    }
+}
