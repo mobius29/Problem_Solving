3b52ccb84331fc1ec00632d72bbcd4613d5732fc
mobius29 <sagjin0000@naver.com>
2023-08-24 11:50:04 +0900

    1107. remote controller

---
+++ Baekjoon/rust/G5/1107/main.rs
@@ -0,0 +1,170 @@
+use std::io::Write;
+use std::str::SplitAsciiWhitespace;
+use std::{fmt, io};
+
+use std::cmp::min;
+use std::collections::VecDeque;
+
+struct Channel {
+    current: usize,
+    count: usize,
+}
+
+fn solve(n: usize, forbidden_buttons: [bool; 10]) -> usize {
+    let mut answer: usize = 500_001;
+    let mut deque: VecDeque<Channel> = VecDeque::new();
+    forbidden_buttons
+        .iter()
+        .enumerate()
+        .for_each(|(idx, is_forbidden)| {
+            if !is_forbidden {
+                deque.push_back(Channel {
+                    current: idx,
+                    count: 1,
+                });
+            }
+        });
+
+    let mut visited = [false; 1_000_001];
+
+    while !deque.is_empty() {
+        let Channel { current, count } = deque.pop_front().unwrap();
+        let added_count = current.abs_diff(n) + count;
+        answer = min(answer, added_count);
+
+        let count = count + 1;
+
+        forbidden_buttons
+            .iter()
+            .enumerate()
+            .for_each(|(idx, is_forbidden)| {
+                if !is_forbidden {
+                    let current = current * 10 + idx;
+                    if current <= 1_000_000 && !visited[current] {
+                        deque.push_back(Channel { current, count });
+                        visited[current] = true;
+                    }
+                }
+            });
+    }
+
+    answer
+}
+
+fn main() {
+    let stdout = io::stdout();
+    let buf_write = &mut io::BufWriter::new(stdout.lock());
+    let line = &mut read();
+
+    let n: usize = line.next();
+    let m: usize = line.next();
+    let mut forbidden_buttons = [false; 10];
+    line.next_iter()
+        .take(m)
+        .for_each(|button: usize| forbidden_buttons[button] = true);
+
+    let answer = min(n.abs_diff(100usize), solve(n, forbidden_buttons));
+
+    writeln!(buf_write, "{}", answer).ok();
+
+    // flush buffer
+    buf_write.flush().unwrap();
+}
+
+fn read() -> Tokenizer<SplitAsciiWhitespace<'static>> {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let str: &'static str = Box::leak(buf.into_boxed_str());
+
+    Tokenizer::new(str, |s| s.split_ascii_whitespace())
+}
+
+// Define Errors For Input
+pub enum InputError<'t> {
+    InputExhaust,
+    ParseError(&'t str),
+}
+
+impl<'t> fmt::Debug for InputError<'t> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputError::InputExhaust => f.debug_struct("InputExhaust").finish(),
+            InputError::ParseError(s) => f.debug_struct("ParseError").field("str", s).finish(),
+        }
+    }
+}
+
+// Implements macros for parse string to certain type
+pub trait Atom: Sized {
+    fn parse_from(s: &str) -> Result<Self, InputError>;
+}
+
+trait IterParse: Sized {
+    fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>>
+    where
+        It: Iterator<Item = &'t str>;
+}
+
+macro_rules! impl_trait_for_fromstr {
+    ($($t:ty) *) => { $(
+        impl Atom for $t {
+            fn parse_from(s: &str) -> Result<Self, InputError> {
+                s.parse().map_err(|_| InputError::ParseError(s))
+            }
+        }
+
+        impl IterParse for $t {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                it.next().map_or( Err(InputError::InputExhaust), <Self as Atom>::parse_from )
+            }
+        }
+    )* };
+}
+
+impl_trait_for_fromstr!(bool char String);
+impl_trait_for_fromstr!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64);
+
+macro_rules! impl_iterparse_for_tuple {
+    ($($t:ident) *) => {
+        impl<$($t),*> IterParse for ($($t),*) where $($t: IterParse),* {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                Ok(( $($t::parse_from(it)?),* ))
+            }
+        }
+    };
+}
+
+impl_iterparse_for_tuple!(A B);
+impl_iterparse_for_tuple!(A B C);
+impl_iterparse_for_tuple!(A B C D);
+
+// Implements Tokenizer for split string line to variables
+struct Tokenizer<It> {
+    it: It,
+}
+
+impl<'arg, 'str: 'arg, It> Tokenizer<It> {
+    pub fn new(s: &'str str, split: impl FnOnce(&'arg str) -> It) -> Self {
+        Self { it: split(s) }
+    }
+}
+
+impl<'t, It> Tokenizer<It>
+where
+    It: Iterator<Item = &'t str>,
+{
+    pub fn next<T: IterParse>(&mut self) -> T {
+        T::parse_from(&mut self.it).unwrap()
+    }
+
+    pub fn next_str(&mut self) -> &'t str {
+        self.it.next().unwrap()
+    }
+
+    pub fn next_ok<T: IterParse>(&mut self) -> Result<T, InputError<'t>> {
+        T::parse_from(&mut self.it)
+    }
+
+    pub fn next_iter<T: IterParse>(&mut self) -> impl Iterator<Item = T> + '_ {
+        std::iter::repeat_with(move || self.next_ok().ok()).map_while(|x| x)
+    }
+}
