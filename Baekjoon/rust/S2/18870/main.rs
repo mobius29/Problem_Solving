7859cdc56ec608b6034b3c229dd1c859b98a8f5e
mobius29 <sagjin0000@naver.com>
2023-09-05 11:43:05 +0900

    18870. coordinate compression

---
+++ Baekjoon/rust/S2/18870/main.rs
@@ -0,0 +1,150 @@
+// https://bamgoesn.github.io/rust-ps-md/misc/fastio.html
+use std::io::Write;
+use std::str::SplitAsciiWhitespace;
+use std::{fmt, io};
+
+use std::cmp::{max, min};
+use std::collections::VecDeque;
+
+const ADJACENT: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+fn lower_bound(array: &Vec<i32>, key: i32) -> usize {
+    let mut left: i32 = 0;
+    let mut right: i32 = array.len() as i32 - 1;
+
+    let mut idx = 0usize;
+
+    while left <= right {
+        let m = (left + right) as usize / 2;
+        if array[m] >= key {
+            if array[m] == key {
+                idx = m;
+            }
+            if m == 0 {
+                break;
+            }
+            right = (m - 1) as i32;
+        } else {
+            left = (m + 1) as i32;
+        }
+    }
+
+    idx
+}
+
+fn main() {
+    let stdout = io::stdout();
+    let buf_write = &mut io::BufWriter::new(stdout.lock());
+    let line = &mut read();
+
+    let n: usize = line.next();
+    let array: Vec<i32> = line.next_iter().take(n).collect();
+    let mut sorted_array = array.clone();
+    sorted_array.sort();
+    sorted_array.dedup();
+
+    for num in array.iter() {
+        let idx = lower_bound(&sorted_array, *num);
+        write!(buf_write, "{} ", idx).ok();
+    }
+
+    buf_write.flush().unwrap();
+}
+
+fn read() -> Tokenizer<SplitAsciiWhitespace<'static>> {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let str: &'static str = Box::leak(buf.into_boxed_str());
+
+    Tokenizer::new(str, |s| s.split_ascii_whitespace())
+}
+
+// Define Errors For Input
+pub enum InputError<'t> {
+    InputExhaust,
+    ParseError(&'t str),
+}
+
+impl<'t> fmt::Debug for InputError<'t> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputError::InputExhaust => f.debug_struct("InputExhaust").finish(),
+            InputError::ParseError(s) => f.debug_struct("ParseError").field("str", s).finish(),
+        }
+    }
+}
+
+// Implements macros for parse string to certain type
+pub trait Atom: Sized {
+    fn parse_from(s: &str) -> Result<Self, InputError>;
+}
+
+trait IterParse: Sized {
+    fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>>
+    where
+        It: Iterator<Item = &'t str>;
+}
+
+macro_rules! impl_trait_for_fromstr {
+    ($($t:ty) *) => { $(
+        impl Atom for $t {
+            fn parse_from(s: &str) -> Result<Self, InputError> {
+                s.parse().map_err(|_| InputError::ParseError(s))
+            }
+        }
+
+        impl IterParse for $t {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                it.next().map_or( Err(InputError::InputExhaust), <Self as Atom>::parse_from )
+            }
+        }
+    )* };
+}
+
+impl_trait_for_fromstr!(bool char String);
+impl_trait_for_fromstr!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64);
+
+macro_rules! impl_iterparse_for_tuple {
+    ($($t:ident) *) => {
+        impl<$($t),*> IterParse for ($($t),*) where $($t: IterParse),* {
+            fn parse_from<'s, 't: 's, It>(it: &'s mut It) -> Result<Self, InputError<'t>> where It: Iterator<Item = &'t str> {
+                Ok(( $($t::parse_from(it)?),* ))
+            }
+        }
+    };
+}
+
+impl_iterparse_for_tuple!(A B);
+impl_iterparse_for_tuple!(A B C);
+impl_iterparse_for_tuple!(A B C D);
+
+// Implements Tokenizer for split string line to variables
+struct Tokenizer<It> {
+    it: It,
+}
+
+impl<'arg, 'str: 'arg, It> Tokenizer<It> {
+    pub fn new(s: &'str str, split: impl FnOnce(&'arg str) -> It) -> Self {
+        Self { it: split(s) }
+    }
+}
+
+impl<'t, It> Tokenizer<It>
+where
+    It: Iterator<Item = &'t str>,
+{
+    pub fn next<T: IterParse>(&mut self) -> T {
+        T::parse_from(&mut self.it).unwrap()
+    }
+
+    pub fn next_str(&mut self) -> &'t str {
+        self.it.next().unwrap()
+    }
+
+    pub fn next_ok<T: IterParse>(&mut self) -> Result<T, InputError<'t>> {
+        T::parse_from(&mut self.it)
+    }
+
+    pub fn next_iter<T: IterParse>(&mut self) -> impl Iterator<Item = T> + '_ {
+        std::iter::repeat_with(move || self.next_ok().ok()).map_while(|x| x)
+    }
+}
